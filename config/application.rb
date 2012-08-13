9456ba6b9ce693bfc154281b3b2d7b4f2afff98d
chenhuiz <zhaichenhui@gmail.com>
2012-08-13 01:05:45 -0400

    create DB and generate devise:User modle

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,11 @@ module DemoApp
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # If you are deploying Rails 3.1 on Heroku, you may want to set:
+    # forcing your application to not access the DB
+    # or load models when precompiling your assets.
+    config.assets.initialize_on_precompile = false
+
   end
 end
